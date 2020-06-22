a7ea6e98d7646983cdeca08b8db038cf280a3c75
Ryan Dewell <ryan@dewell.org>
2020-06-22 13:23:09 -0600

    Updates

--- components/button.js
+++ components/button.js
@@ -12,6 +12,8 @@ class UIMenuButton extends HTMLElement{
 
     connectedCallback(){
         this.setAttribute('tabindex', '0');
+        //this.setAttribute('aria-haspopup', 'true');
+        //this.setAttribute('aria-controls', 'ui-menu');
     }
 }
 window.customElements.define('ui-menu-button', UIMenuButton);

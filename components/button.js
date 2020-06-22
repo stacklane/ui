c686c1e54325377177f6c4190686fa8ac20272d7
Ryan Dewell <ryan@dewell.org>
2020-06-22 15:37:21 -0600

    Updates

--- components/button.js
+++ components/button.js
@@ -2,18 +2,41 @@
 
 class UIButton extends HTMLElement{
 
-    connectedCallback(){
+    constructor() {
+        super();
         this.setAttribute('tabindex', '0');
     }
+
 }
 window.customElements.define('ui-button', UIButton);
 
 class UIMenuButton extends HTMLElement{
 
-    connectedCallback(){
+    constructor() {
+        super();
         this.setAttribute('tabindex', '0');
         //this.setAttribute('aria-haspopup', 'true');
         //this.setAttribute('aria-controls', 'ui-menu');
+        const that = this;
+        this.addEventListener('keydown', function(event) {
+            if (event.key === 'Escape') that._esc(event);
+        });
     }
+
+    /**
+     * Uses :focus/:focus-within to show the menu, therefore Escape key should unfocus/hide menu.
+     */
+    _esc(event){
+        this.blur();
+        event.preventDefault();
+    }
+
 }
 window.customElements.define('ui-menu-button', UIMenuButton);
+
+class UIMenu extends HTMLElement{
+    constructor() {
+        super();
+    }
+}
+window.customElements.define('ui-menu', UIMenuButton);

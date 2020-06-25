28cd942034ae1f3fdca296adf7b65e95242fec83
Ryan Dewell <ryan@dewell.org>
2020-06-25 14:11:17 -0600

    Updates

--- components/bar.js
+++ components/bar.js
@@ -1,7 +1,13 @@
+/**
+ * @see _bar.scss
+ */
 class UIBar extends HTMLElement{
     constructor() {
         super();
-
+    }
+    isPath(){
+        this.classList.add('is-path');
+        return this;
     }
 }
 window.customElements.define('ui-bar', UIBar);

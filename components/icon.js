bf3aa146755bbc788f5512261ac4e70dde6a890f
Ryan Dewell <ryan@dewell.org>
2020-06-25 17:59:10 -0600

    Updates

--- components/icon.js
+++ components/icon.js
@@ -1,13 +1,12 @@
 class UIIcon extends HTMLElement{
+    static X = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>';
+    static PLUS = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>';
+
+    static x(){
+        return new UIIcon(UIIcon.X);
+    }
     static plus(){
-        const i = new UIIcon();
-        i.classList.add('is-plus');
-        return i;
-    };
-    static plusSmall(){
-        const i = new UIIcon();
-        i.classList.add('is-plus-small');
-        return i;
+        return new UIIcon(UIIcon.PLUS);
     };
     constructor(string) {
         super();

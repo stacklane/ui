922f8c7c9e673e6b85cb94797982b37211fe179c
Ryan Dewell <ryan@dewell.org>
2020-06-25 18:09:33 -0600

    Updates

--- components/icon.js
+++ components/icon.js
@@ -1,7 +1,13 @@
 class UIIcon extends HTMLElement{
+    /**
+     * Un-opinionated 45 degree crossed lines.
+     */
     static x(){
         return new UIIcon('<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>');
     }
+    /**
+     * Un-opinionated crossed lines.
+     */
     static plus(){
         return new UIIcon('<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>');
     };

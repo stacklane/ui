96b41436ebbebf817bc9d8878fb8f41476607445
Ryan Dewell <ryan@dewell.org>
2020-06-27 09:34:15 -0600

    Updates

--- components/spinner.js
+++ components/spinner.js
@@ -3,9 +3,8 @@ class UISpinner extends HTMLElement{
     constructor() {
         super();
     }
-
     connectedCallback(){
         this.innerHTML = '<div></div><div></div><div></div><div></div>';
     }
 }
-window.customElements.define('ui-spinner', UIBar);
+window.customElements.define('ui-spinner', UISpinner);

72b889c57e4ef7e6312817a0230b6bb53bf97e24
Ryan Dewell <ryan@dewell.org>
2020-06-29 11:14:10 -0600

    Updates

--- base/app.js
+++ base/app.js
@@ -8,7 +8,7 @@ const _UI_ICON_SVG_MENU = '<svg xmlns="http://www.w3.org/2000/svg" height="24" v
 const _UI_ICON_SVG_MENU_CLOSE = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z"/></svg>';
 
 /**
- * May be extended.
+ * May be subclassed.
  */
 class UIScrollable extends HTMLElement{
     constructor(type) {
@@ -16,10 +16,11 @@ class UIScrollable extends HTMLElement{
         this._type = type;
     }
     connectedCallback(){
+        this.classList.add('ui-scrollable');
         switch (this._type){
-            case 'x':{this.classList.add('ui-scrollable-x'); break;}
-            case 'xy':{this.classList.add('ui-scrollable-xy'); break;}
-            default:{this.classList.add('ui-scrollable-y'); break;}
+            case 'x':{this.classList.add('is-x'); break;}
+            case 'xy':{this.classList.add('is-xy'); break;}
+            default:{this.classList.add('is-y'); break;}
         }
     }
 }

00436a347bae84d205da3d26f98d655cce93665c
Ryan Dewell <ryan@dewell.org>
2020-07-02 10:09:46 -0600

    ui-bar is-column

--- base/bar.js
+++ base/bar.js
@@ -7,6 +7,7 @@ class UIBar extends HTMLElement{
     }
     _cls(c){ this.classList.add(c); return this; }
     path(){return this._cls('is-path');}
+    column(){return this._cls('is-column');}
     grow(){return this._cls('is-grow');}
     growEnd(){return this._cls('is-end')._cls('is-grow');}
     growEven(){return this._cls('is-even')._cls('is-grow');}

18c43491448a0269f15082f5ec8af2908011e6e1
Ryan Dewell <ryan@dewell.org>
2020-07-08 10:34:39 -0600

    button/tab work

--- base/app.js
+++ base/app.js
@@ -7,6 +7,20 @@ const _UI_ICON_SVG_PLUS = '<svg xmlns="http://www.w3.org/2000/svg" height="24" v
 const _UI_ICON_SVG_MENU = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>';
 const _UI_ICON_SVG_MENU_CLOSE = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z"/></svg>';
 
+class UISpinner extends HTMLElement{
+    constructor() {
+        super();
+    }
+    connectedCallback(){
+        this.innerHTML = '<div></div><div></div><div></div><div></div>';
+    }
+    activated(){
+        this.classList.add('is-activated');
+        return this;
+    }
+}
+window.customElements.define('ui-spinner', UISpinner);
+
 /**
  * May be subclassed.
  */
